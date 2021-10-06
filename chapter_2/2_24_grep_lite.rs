de883cccf1d37123e69a45c83bca2d057670be29
jkachmar <git@jkachmar.com>
2021-10-06 00:27:34 -0400

    Removes unwrap

--- chapter_2/2_24_grep_lite.rs
+++ chapter_2/2_24_grep_lite.rs
@@ -1,13 +1,14 @@
 use regex::Regex;
 fn main() {
-    let re = Regex::new("picture").unwrap();
-
     let quote = "Every face, every shop, bedroom window, public-house, and
  dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
-    for line in quote.lines() {
-        if re.find(line).is_some() {
-            println!("{}", line);
+
+    if let Ok(re) = Regex::new("picture") {
+        for line in quote.lines() {
+            if re.find(line).is_some() {
+                println!("{}", line);
+            }
         }
     }
 }
